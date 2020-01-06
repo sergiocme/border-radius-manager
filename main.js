b9cb573f2c0580ef79f617d37fbb7e41b2efa0e6
Sérgio Carvalho <sergiocarvalho.m@outlook.com>
2020-01-06 14:29:13 -0300

    Set element properties to inputs

---
+++ main.js
@@ -0,0 +1,9 @@
+const contentPreview = document.getElementsByClassName('content-preview')[0];
+const heightInput = document.getElementById('height');
+const widthInput = document.getElementById('width');
+const backgroundColorInput = document.getElementById('background-color');
+
+heightInput.value = getComputedStyle(contentPreview).height;
+widthInput.value = getComputedStyle(contentPreview).width;
+backgroundColorInput.value = getComputedStyle(contentPreview).background;
+
