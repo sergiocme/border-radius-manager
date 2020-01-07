f10bf64d64fdba44c6b62b9341b84cac3d939524
Sérgio Carvalho <sergiocarvalho.m@outlook.com>
2020-01-07 14:36:41 -0300

    Bind css propperties to content preview

--- main.js
+++ main.js
@@ -6,3 +6,11 @@ const backgroundColorInput = document.getElementById('background-color');
 heightInput.value = getComputedStyle(contentPreview).height;
 widthInput.value = getComputedStyle(contentPreview).width;
 backgroundColorInput.value = getComputedStyle(contentPreview).backgroundColor;
+
+heightInput.addEventListener('input', changeCssProperty);
+widthInput.addEventListener('input', changeCssProperty);
+backgroundColorInput.addEventListener('input', changeCssProperty);
+
+function changeCssProperty(event) {
+  contentPreview.style[event.target.id] = event.target.value;
+}
